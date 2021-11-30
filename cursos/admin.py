4f712e745a00584b773cd54a1f4e32890717ab51
Solcii <sol081105@gmail.com>
2021-11-30 00:28:34 -0300

    creacion modelos

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Profesor
+from .models import Curso
 
-# Register your models here.
+admin.site.register(Profesor)
+admin.site.register(Curso)
--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class Profesor(models.Model):
+    dni = models.CharField('DNI', max_length=8, primary_key=True)
+    apellido = models.CharField('Apellido', max_length=50, null=False, blank=False)
+    nombre = models.CharField('Nombre', max_length=50, null=False, blank=False)
+    fecha_nacimiento = models.DateField('Fecha de nacimiento')
+    email = models.EmailField('Correo electrónico', null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Profesor'
+        verbose_name_plural = 'Profesores'
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
+
+class Curso(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField('Titulo del curso', max_length=100, null=False, blank=False)
+    idiomas = [
+        ('I', 'Inglés'),
+        ('A', 'Alemán'),
+        ('F', 'Frances'),
+        ('P', 'Portugués')
+    ]
+    idioma = models.CharField(max_length=1, choices=idiomas, default='I')
+    niveles = [
+        ('P', 'Principiante'),
+        ('I', 'Intermedio'),
+        ('A', 'Avanzado')
+    ]
+    nivel = models.CharField(max_length=1, choices=niveles, default='P')
+    duracion = models.PositiveIntegerField('Duración (días)', default=30)
+    modalidades = [
+        ('P', 'Presencial'),
+        ('V', 'Virtual')
+    ]
+    modalidad = models.CharField(max_length=1, choices=modalidades, default='P')
+    profesor = models.ForeignKey(Profesor, null=False, blank=False, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+
+    def __str__(self):
+        return f'{self.titulo}'
+
+
