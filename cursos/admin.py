1db1308500c634d56f72eb32786c1b7c21fbd057
Solcii <sol081105@gmail.com>
2021-11-30 19:57:18 -0300

    personalizacion admin

--- cursos/admin.py
+++ cursos/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Profesor
 from .models import Curso
 
-admin.site.register(Profesor)
-admin.site.register(Curso)
+
+class ProfesorAdmin(admin.ModelAdmin):
+    search_fields = ['nombre', 'apellido']
+    list_display = ('nombre', 'apellido', 'email',)
+
+class CursoAdmin(admin.ModelAdmin):
+    search_fields = ['titulo']
+    list_display = ('titulo', 'idioma', 'nivel', 'modalidad',)
+
+admin.site.register(Profesor, ProfesorAdmin)
+admin.site.register(Curso, CursoAdmin)
