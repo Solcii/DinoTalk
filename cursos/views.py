46e7708f80b8a700354b282300c56e7706a39a21
Solcii <sol081105@gmail.com>
2021-12-04 01:18:50 -0300

    barra de busqueda por nombre

--- cursos/views.py
+++ cursos/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.template import loader
 from .models import Curso
+from django.db.models import Q
 
 def home(request):
     return render(request, 'index.html')
 
 def cursos(request):
-    cursos = Curso.objects.filter()
+    queryset = request.GET.get('buscar')
+    cursos = Curso.objects.all()
+    if queryset:
+        cursos = Curso.objects.filter(Q(titulo__icontains = queryset))
     return render(request, 'cursos.html', {'cursos': cursos} )
 
 def contacto(request):
