bc91d94a2e8bdbe0750ba32201fdf194d85ac682
Solcii <sol081105@gmail.com>
2021-12-03 22:10:08 -0300

    repeticion card cursos

--- cursos/views.py
+++ cursos/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.template import loader
+from .models import Curso
 
 def home(request):
     return render(request, 'index.html')
 
 def cursos(request):
-    return render(request, 'cursos.html')
+    cursos = Curso.objects.filter()
+    return render(request, 'cursos.html', {'cursos': cursos} )
 
 def contacto(request):
     return render(request, 'contacto.html')
