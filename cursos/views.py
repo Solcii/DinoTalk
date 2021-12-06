d09e3b6bbdfdc04584bdc1b34fa7e7c0041e6325
Solcii <sol081105@gmail.com>
2021-12-06 19:21:39 -0300

    sistema filtros base

--- cursos/views.py
+++ cursos/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.template import loader
 from .models import Curso
-from django.db.models import Q
+from django.db.models import Q, query
 
 def home(request):
     return render(request, 'index.html')
 
 def cursos(request):
-    queryset = request.GET.get('buscar')
     cursos = Curso.objects.all()
+
+    queryset = request.GET.get('buscar')
     if queryset:
         cursos = Curso.objects.filter(Q(titulo__icontains = queryset))
+
+    idioma_seleccionado = request.GET.get('idioma')
+    if idioma_seleccionado and idioma_seleccionado != 'Seleccionar...':
+        cursos = cursos.filter(idioma=idioma_seleccionado)
+
+    nivel_seleccionado = request.GET.get('nivel')
+    if nivel_seleccionado and nivel_seleccionado != 'Seleccionar...':
+        cursos = cursos.filter(nivel=nivel_seleccionado)
+
     return render(request, 'cursos.html', {'cursos': cursos} )
 
 def contacto(request):
